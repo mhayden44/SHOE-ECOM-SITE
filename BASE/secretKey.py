86a2adc8e0a9bdddb17734aaafc3ffbca6fbdf18
mhayden44 <mhayden44@ivytech.edu>
2022-12-14 12:30:19 -0600

    commit

---
+++ BASE/secretKey.py
@@ -0,0 +1,8 @@
+import secrets
+
+length = 50
+chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+
+secret_key = ''.join(secrets.choice(chars) for i in range(length))
+
+print(secret_key)
